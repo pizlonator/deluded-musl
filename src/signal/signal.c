c9b1071394dd4b94be4cc67793910d28f448fe7d
Filip Pizlo <pizlo@behemoth.local>
2024-01-14 20:24:09 -0800

    more shit

--- src/signal/signal.c
+++ src/signal/signal.c
@@ -1,12 +1,10 @@
 #include <signal.h>
 #include "syscall.h"
+#include <stdfil.h>
 
 void (*signal(int sig, void (*func)(int)))(int)
 {
-	struct sigaction sa_old, sa = { .sa_handler = func, .sa_flags = SA_RESTART };
-	if (__sigaction(sig, &sa, &sa_old) < 0)
-		return SIG_ERR;
-	return sa_old.sa_handler;
+    return zsys_signal(sig, func);
 }
 
 weak_alias(signal, bsd_signal);

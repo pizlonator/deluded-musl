9ee3807f2ac7e7620bfd97640c38d447d89cfef0
Filip Pizlo <pizlo@behemoth.local>
2024-01-18 20:47:23 -0800

    more shit

--- src/fcntl/fcntl.c
+++ src/fcntl/fcntl.c
@@ -3,46 +3,32 @@
 #include <stdarg.h>
 #include <errno.h>
 #include "syscall.h"
+#include <stdfil.h>
 
 int fcntl(int fd, int cmd, ...)
 {
-	unsigned long arg;
 	va_list ap;
+        int res;
 	va_start(ap, cmd);
-	arg = va_arg(ap, unsigned long);
+        // FIXME: I should totally add varargs forwarding support to the stdfil API, it would be
+        // hella fucking useful.
+        switch (cmd) {
+        case F_GETLK:
+        case F_SETLK:
+        case F_SETLKW:
+            res = zsys_fcntl(fd, cmd, va_arg(ap, void*));
+            break;
+        case F_DUPFD:
+        case F_SETFD:
+        case F_SETFL:
+        case F_SETOWN:
+        case F_DUPFD_CLOEXEC:
+            res = zsys_fcntl(fd, cmd, va_arg(ap, int));
+            break;
+        default:
+            res = zsys_fcntl(fd, cmd);
+            break;
+        }
 	va_end(ap);
-	if (cmd == F_SETFL) arg |= O_LARGEFILE;
-	if (cmd == F_SETLKW) return syscall_cp(SYS_fcntl, fd, cmd, (void *)arg);
-	if (cmd == F_GETOWN) {
-		struct f_owner_ex ex;
-		int ret = __syscall(SYS_fcntl, fd, F_GETOWN_EX, &ex);
-		if (ret == -EINVAL) return __syscall(SYS_fcntl, fd, cmd, (void *)arg);
-		if (ret) return __syscall_ret(ret);
-		return ex.type == F_OWNER_PGRP ? -ex.pid : ex.pid;
-	}
-	if (cmd == F_DUPFD_CLOEXEC) {
-		int ret = __syscall(SYS_fcntl, fd, F_DUPFD_CLOEXEC, arg);
-		if (ret != -EINVAL) {
-			if (ret >= 0)
-				__syscall(SYS_fcntl, ret, F_SETFD, FD_CLOEXEC);
-			return __syscall_ret(ret);
-		}
-		ret = __syscall(SYS_fcntl, fd, F_DUPFD_CLOEXEC, 0);
-		if (ret != -EINVAL) {
-			if (ret >= 0) __syscall(SYS_close, ret);
-			return __syscall_ret(-EINVAL);
-		}
-		ret = __syscall(SYS_fcntl, fd, F_DUPFD, arg);
-		if (ret >= 0) __syscall(SYS_fcntl, ret, F_SETFD, FD_CLOEXEC);
-		return __syscall_ret(ret);
-	}
-	switch (cmd) {
-	case F_SETLK:
-	case F_GETLK:
-	case F_GETOWN_EX:
-	case F_SETOWN_EX:
-		return syscall(SYS_fcntl, fd, cmd, (void *)arg);
-	default:
-		return syscall(SYS_fcntl, fd, cmd, arg);
-	}
+        return res;
 }

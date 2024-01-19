650ef25d8a1aaaea6229f49c998bc22b8032a169
Filip Pizlo <pizlo@behemoth.local>
2024-01-19 08:17:01 -0800

    more shit

--- src/stdio/__fdopen.c
+++ src/stdio/__fdopen.c
@@ -28,13 +28,13 @@ FILE *__fdopen(int fd, const char *mode)
 	if (!strchr(mode, '+')) f->flags = (*mode == 'r') ? F_NOWR : F_NORD;
 
 	/* Apply close-on-exec flag */
-	if (strchr(mode, 'e')) __syscall(SYS_fcntl, fd, F_SETFD, FD_CLOEXEC);
+	if (strchr(mode, 'e')) zsys_fcntl(fd, F_SETFD, FD_CLOEXEC);
 
 	/* Set append mode on fd if opened for append */
 	if (*mode == 'a') {
-		int flags = __syscall(SYS_fcntl, fd, F_GETFL);
+		int flags = zsys_fcntl(fd, F_GETFL);
 		if (!(flags & O_APPEND))
-			__syscall(SYS_fcntl, fd, F_SETFL, flags | O_APPEND);
+			zsys_fcntl(fd, F_SETFL, flags | O_APPEND);
 		f->flags |= F_APP;
 	}
 
@@ -44,7 +44,7 @@ FILE *__fdopen(int fd, const char *mode)
 
 	/* Activate line buffered mode for terminals */
 	f->lbf = EOF;
-	if (!(f->flags & F_NOWR) && !__syscall(SYS_ioctl, fd, TIOCGWINSZ, &wsz))
+	if (!(f->flags & F_NOWR) && !zsys_ioctl(fd, TIOCGWINSZ, &wsz))
 		f->lbf = '\n';
 
 	/* Initialize op ptrs. No problem if some are unneeded. */

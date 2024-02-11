a11dfc7dff41c5bedd0a20c62fcf86fa64a69f5d
Filip Pizlo <pizlo@behemoth.local>
2024-02-11 13:03:47 -0800

    more shit

--- src/network/getsockname.c
+++ src/network/getsockname.c
@@ -1,7 +1,8 @@
 #include <sys/socket.h>
 #include "syscall.h"
+#include <stdfil.h>
 
 int getsockname(int fd, struct sockaddr *restrict addr, socklen_t *restrict len)
 {
-	return socketcall(getsockname, fd, addr, len, 0, 0, 0);
+    return zsys_getsockname(fd, addr, len);
 }

c6bbc3783c79f9606574718cda1635e200342c7f
Filip Pizlo <pizlo@behemoth.local>
2024-02-11 18:30:34 -0800

    more shit

--- src/network/getpeername.c
+++ src/network/getpeername.c
@@ -1,7 +1,8 @@
 #include <sys/socket.h>
 #include "syscall.h"
+#include <stdfil.h>
 
 int getpeername(int fd, struct sockaddr *restrict addr, socklen_t *restrict len)
 {
-	return socketcall(getpeername, fd, addr, len, 0, 0, 0);
+    return zsys_getpeername(fd, addr, len);
 }

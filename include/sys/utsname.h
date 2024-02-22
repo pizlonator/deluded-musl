955d32b75cbc7510cd488057a93e29b8e23cdcd2
Filip Pizlo <pizlo@behemoth.local>
2024-02-21 17:40:38 -0800

    more shit

--- include/sys/utsname.h
+++ include/sys/utsname.h
@@ -13,6 +13,11 @@ struct utsname {
 	char release[65];
 	char version[65];
 	char machine[65];
+#ifdef _GNU_SOURCE
+	char domainname[65];
+#else
+	char __domainname[65];
+#endif
 };
 
 int uname (struct utsname *);

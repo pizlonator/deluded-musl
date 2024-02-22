e66b9d0bd21bd543b31dca01bb04fd405cd414f5
Filip Pizlo <pizlo@behemoth.local>
2024-02-21 17:25:01 -0800

    more shit

--- src/string/strlcpy.c
+++ src/string/strlcpy.c
@@ -14,19 +14,6 @@ size_t strlcpy(char *d, const char *s, size_t n)
 	size_t *wd;
 
 	if (!n--) goto finish;
-#ifdef __GNUC__
-	typedef size_t __attribute__((__may_alias__)) word;
-	const word *ws;
-	if (((uintptr_t)s & ALIGN) == ((uintptr_t)d & ALIGN)) {
-		for (; ((uintptr_t)s & ALIGN) && n && (*d=*s); n--, s++, d++);
-		if (n && *s) {
-			wd=(void *)d; ws=(const void *)s;
-			for (; n>=sizeof(size_t) && !HASZERO(*ws);
-			       n-=sizeof(size_t), ws++, wd++) *wd = *ws;
-			d=(void *)wd; s=(const void *)ws;
-		}
-	}
-#endif
 	for (; n && (*d=*s); n--, s++, d++);
 	*d = 0;
 finish:

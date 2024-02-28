85dd08db8b8e1eb901b19dddc77a8930e3a6f964
Filip Pizlo <pizlo@behemoth.local>
2024-02-27 18:03:53 -0800

    more shit

--- src/unistd/dup2.c
+++ src/unistd/dup2.c
@@ -2,19 +2,9 @@
 #include <errno.h>
 #include <fcntl.h>
 #include "syscall.h"
+#include <stdfil.h>
 
 int dup2(int old, int new)
 {
-	int r;
-#ifdef SYS_dup2
-	while ((r=__syscall(SYS_dup2, old, new))==-EBUSY);
-#else
-	if (old==new) {
-		r = __syscall(SYS_fcntl, old, F_GETFD);
-		if (r >= 0) return old;
-	} else {
-		while ((r=__syscall(SYS_dup3, old, new, 0))==-EBUSY);
-	}
-#endif
-	return __syscall_ret(r);
+    return zsys_dup2(old, new);
 }

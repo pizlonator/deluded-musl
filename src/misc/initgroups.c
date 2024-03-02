ff972e49e366a2f3ce45b8750aa4f89fcf4934b7
Filip Pizlo <pizlo@behemoth.local>
2024-03-02 15:59:04 -0800

    Implement getgroups/getgroup;list/initgroups syscalls. Corresponds to llvm-project-deluge/6e399e9fdb94

--- src/misc/initgroups.c
+++ src/misc/initgroups.c
@@ -1,11 +1,9 @@
 #define _GNU_SOURCE
 #include <grp.h>
 #include <limits.h>
+#include <stdfil.h>
 
 int initgroups(const char *user, gid_t gid)
 {
-	gid_t groups[NGROUPS_MAX];
-	int count = NGROUPS_MAX;
-	if (getgrouplist(user, gid, groups, &count) < 0) return -1;
-	return setgroups(count, groups);
+    return zsys_initgroups(user, gid);
 }

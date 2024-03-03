de0f47bdcde93699c162236dabd035c7cdb7dc19
Filip Pizlo <pizlo@behemoth.local>
2024-03-03 10:56:23 -0800

    Add chown/chmod syscalls. Corresponds to llvm-project-deluge/369786cdb1b1

--- src/unistd/chown.c
+++ src/unistd/chown.c
@@ -4,9 +4,5 @@
 
 int chown(const char *path, uid_t uid, gid_t gid)
 {
-#ifdef SYS_chown
-	return syscall(SYS_chown, path, uid, gid);
-#else
-	return syscall(SYS_fchownat, AT_FDCWD, path, uid, gid, 0);
-#endif
+    return zsys_chown(path, uid, gid);
 }

61afee7abb7cba0e831c54454d290eaf8058d63e
Filip Pizlo <pizlo@behemoth.local>
2024-03-09 19:45:27 -0800

    Add unlink/link syscalls. Corresponds to llvm-project-deluge/4d3c5c65f963

--- src/unistd/link.c
+++ src/unistd/link.c
@@ -4,9 +4,5 @@
 
 int link(const char *existing, const char *new)
 {
-#ifdef SYS_link
-	return syscall(SYS_link, existing, new);
-#else
-	return syscall(SYS_linkat, AT_FDCWD, existing, AT_FDCWD, new, 0);
-#endif
+    return zsys_link(existing, new);
 }

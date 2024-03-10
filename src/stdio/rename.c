9dd7702c10c5d029e668c1665d19d51f9c685b14
Filip Pizlo <pizlo@behemoth.local>
2024-03-09 16:59:32 -0800

    Implement fchmod and rename syscalls. Corresponds to llvm-project-deluge/41cb3981f72c

--- src/stdio/rename.c
+++ src/stdio/rename.c
@@ -4,11 +4,5 @@
 
 int rename(const char *old, const char *new)
 {
-#if defined(SYS_rename)
-	return syscall(SYS_rename, old, new);
-#elif defined(SYS_renameat)
-	return syscall(SYS_renameat, AT_FDCWD, old, AT_FDCWD, new);
-#else
-	return syscall(SYS_renameat2, AT_FDCWD, old, AT_FDCWD, new, 0);
-#endif
+    return zsys_rename(old, new);
 }

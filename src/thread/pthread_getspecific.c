e63af82ededac569dcf9acbe1e66652e1b01624d
Filip Pizlo <pizlo@behemoth.local>
2024-04-26 17:06:45 -0700

    Support __thread (language thread locals) the cheesiest way possible. Corresponds to llvm-project-deluge/f957a221978a

--- src/thread/pthread_getspecific.c
+++ src/thread/pthread_getspecific.c
@@ -13,3 +13,15 @@ static void *__pthread_getspecific(pthread_key_t k)
 
 weak_alias(__pthread_getspecific, pthread_getspecific);
 weak_alias(__pthread_getspecific, tss_get);
+
+/* This is a total hack to make FilPizlonator.cpp simpler for now. This should die
+   eventually. */
+void* pthread_getspecific_with_default_np(pthread_key_t k, void* (*default_initializer)(void))
+{
+    void* result = pthread_getspecific(k);
+    if (!result) {
+        result = default_initializer();
+        pthread_setspecific(k, result);
+    }
+    return result;
+}

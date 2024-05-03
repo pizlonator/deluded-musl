3ac7d8e674d98e01295c8ddb885508f3eacf3f84
Filip Pizlo <pizlo@behemoth.local>
2024-05-02 20:04:21 -0700

    Fix more of __map_file and add support for mkdirat. Corresponds to llvm-project-deluge/9e56fee21c8a

--- src/stat/mkdir.c
+++ src/stat/mkdir.c
@@ -1,12 +1,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include "syscall.h"
+#include <stdfil.h>
 
 int mkdir(const char *path, mode_t mode)
 {
-#ifdef SYS_mkdir
-	return syscall(SYS_mkdir, path, mode);
-#else
-	return syscall(SYS_mkdirat, AT_FDCWD, path, mode);
-#endif
+    return zsys_mkdirat(AT_FDCWD, path, mode);
 }
--- src/time/__map_file.c
+++ src/time/__map_file.c
@@ -14,6 +14,6 @@ const char unsigned *__map_file(const char *pathname, size_t *size)
 		map = __mmap(0, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
 		*size = st.st_size;
 	}
-	__syscall(SYS_close, fd);
+	zsys_close(fd);
 	return map == MAP_FAILED ? 0 : map;
 }

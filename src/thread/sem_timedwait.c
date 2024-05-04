a00811c89351e6839931697ee01a37c853882f81
Filip Pizlo <pizlo@behemoth.local>
2024-05-03 17:56:04 -0700

    Get sem_timedwait to work.

--- src/thread/sem_timedwait.c
+++ src/thread/sem_timedwait.c
@@ -9,7 +9,7 @@ static void cleanup(void *p)
 
 int sem_timedwait(sem_t *restrict sem, const struct timespec *restrict at)
 {
-	pthread_testcancel();
+	//pthread_testcancel();
 
 	if (!sem_trywait(sem)) return 0;
 
@@ -21,9 +21,9 @@ int sem_timedwait(sem_t *restrict sem, const struct timespec *restrict at)
 		int r, priv = sem->__val[2];
 		a_inc(sem->__val+1);
 		a_cas(sem->__val, 0, 0x80000000);
-		pthread_cleanup_push(cleanup, (void *)(sem->__val+1));
+		//pthread_cleanup_push(cleanup, (void *)(sem->__val+1));
 		r = __timedwait_cp(sem->__val, 0x80000000, CLOCK_REALTIME, at, priv);
-		pthread_cleanup_pop(1);
+		//pthread_cleanup_pop(1);
 		if (r) {
 			errno = r;
 			return -1;

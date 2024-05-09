95dadaea04cb2c003f1f139a20f106e62705ce18
Filip Pizlo <pizlo@behemoth.local>
2024-05-09 15:13:27 -0700

    Implement sigwait. Corresponds to llvm-project-deluge/aeaff8ee333c

--- src/signal/sigwait.c
+++ src/signal/sigwait.c
@@ -1,10 +1,7 @@
 #include <signal.h>
+#include <stdfil.h>
 
 int sigwait(const sigset_t *restrict mask, int *restrict sig)
 {
-	siginfo_t si;
-	if (sigtimedwait(mask, &si, 0) < 0)
-		return -1;
-	*sig = si.si_signo;
-	return 0;
+    return zsys_sigwait(mask, sig);
 }

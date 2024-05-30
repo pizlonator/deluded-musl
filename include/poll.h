16e942dc069fa044a0829170938e74c7f4f393de
Phil Pizlo <pizlo@mac.com>
2024-05-30 00:16:01 +0000

    Hide ppoll, since we don't support it.

--- include/poll.h
+++ include/poll.h
@@ -36,20 +36,6 @@ struct pollfd {
 
 int poll (struct pollfd *, nfds_t, int);
 
-#ifdef _GNU_SOURCE
-#define __NEED_time_t
-#define __NEED_struct_timespec
-#define __NEED_sigset_t
-#include <bits/alltypes.h>
-int ppoll(struct pollfd *, nfds_t, const struct timespec *, const sigset_t *);
-#endif
-
-#if _REDIR_TIME64
-#ifdef _GNU_SOURCE
-__REDIR(ppoll, __ppoll_time64);
-#endif
-#endif
-
 #ifdef __cplusplus
 }
 #endif

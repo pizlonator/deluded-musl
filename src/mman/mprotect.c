164fb5ab8fd147ddbac7b92dc9ffdedcda08c70a
Filip Pizlo <pizlo@daath.pizlohouse.com>
2024-06-30 10:12:40 -0700

    Add mprotect, getpgid, getpgrp. Corresponds to llvm-project-deluge/d300abc56deecc3a1551142a8cb7d7138875079f

--- src/mman/mprotect.c
+++ src/mman/mprotect.c
@@ -4,10 +4,7 @@
 
 int __mprotect(void *addr, size_t len, int prot)
 {
-	size_t start, end;
-	start = (size_t)addr & -PAGE_SIZE;
-	end = (size_t)((char *)addr + len + PAGE_SIZE-1) & -PAGE_SIZE;
-	return syscall(SYS_mprotect, start, end-start, prot);
+    return zsys_mprotect(addr, len, prot);
 }
 
 weak_alias(__mprotect, mprotect);

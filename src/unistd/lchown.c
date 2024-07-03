2ff5df141c0170840980c91acfb20b14e7b495a1
Filip Pizlo <pizlo@daath.pizlohouse.com>
2024-07-02 18:52:37 -0700

    Add fchownat and lchown syscalls. Corresponds to llvm-project-deluge/7b0f1fa8adfb

--- src/unistd/lchown.c
+++ src/unistd/lchown.c
@@ -4,9 +4,5 @@
 
 int lchown(const char *path, uid_t uid, gid_t gid)
 {
-#ifdef SYS_lchown
-	return syscall(SYS_lchown, path, uid, gid);
-#else
-	return syscall(SYS_fchownat, AT_FDCWD, path, uid, gid, AT_SYMLINK_NOFOLLOW);
-#endif
+    return zsys_lchown(path, uid, gid);
 }

d89a2589ac2f42bf310fb8ac9ce3d50997dfe92c
Fil Pizlo <pizlo@mac.com>
2024-07-13 20:32:36 -0700

    Make fopen work completely.

--- src/stdio/fopen.c
+++ src/stdio/fopen.c
@@ -2,7 +2,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
-#include <stdfil.h>
+#include <pizlonated_musl_syscalls.h>
 
 FILE *fopen(const char *restrict filename, const char *restrict mode)
 {
@@ -22,11 +22,11 @@ FILE *fopen(const char *restrict filename, const char *restrict mode)
 	fd = zsys_open(filename, flags, 0666);
 	if (fd < 0) return 0;
 	if (flags & O_CLOEXEC)
-		__syscall(SYS_fcntl, fd, F_SETFD, FD_CLOEXEC);
+		zsys_fcntl(fd, F_SETFD, FD_CLOEXEC);
 
 	f = __fdopen(fd, mode);
 	if (f) return f;
 
-	__syscall(SYS_close, fd);
+	zsys_close(fd);
 	return 0;
 }

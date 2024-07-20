ecc112945e16ad89334c9cf6dbeda51e76beaec3
Fil Pizlo <pizlo@mac.com>
2024-07-20 08:01:55 -0700

    Add an API for removing special signals from sigsets, since libpizlo needs to do that.

--- include/signal.h
+++ include/signal.h
@@ -215,6 +215,7 @@ int kill(pid_t, int);
 int sigemptyset(sigset_t *);
 int sigfillset(sigset_t *);
 int sigaddset(sigset_t *, int);
+int sigdelsetyolo(sigset_t *, int);
 int sigdelset(sigset_t *, int);
 int sigismember(const sigset_t *, int);
 
--- src/signal/sigdelset.c
+++ src/signal/sigdelset.c
@@ -1,13 +1,23 @@
 #include <signal.h>
 #include <errno.h>
 
-int sigdelset(sigset_t *set, int sig)
+int sigdelsetyolo(sigset_t *set, int sig)
 {
 	unsigned s = sig-1;
-	if (s >= _NSIG-1 || sig-32U < 3) {
+	if (s >= _NSIG-1) {
 		errno = EINVAL;
 		return -1;
 	}
 	set->__bits[s/8/sizeof *set->__bits] &=~(1UL<<(s&8*sizeof *set->__bits-1));
 	return 0;
+}
+
+int sigdelset(sigset_t *set, int sig)
+{
+	unsigned s = sig-1;
+	if (sig-32U < 3) {
+		errno = EINVAL;
+		return -1;
+	}
+	return sigdelsetyolo(set, sig);
 }

6c78baae60c19b4117a2482a544074275e583c07
Fil Pizlo <pizlo@mac.com>
2024-07-26 07:42:04 -0700

    Add support for remove system function and remove dead code from ioctl.

--- src/stdio/remove.c
+++ src/stdio/remove.c
@@ -5,15 +5,7 @@
 
 int remove(const char *path)
 {
-#ifdef SYS_unlink
-	int r = __syscall(SYS_unlink, path);
-#else
-	int r = __syscall(SYS_unlinkat, AT_FDCWD, path, 0);
-#endif
-#ifdef SYS_rmdir
-	if (r==-EISDIR) r = __syscall(SYS_rmdir, path);
-#else
-	if (r==-EISDIR) r = __syscall(SYS_unlinkat, AT_FDCWD, path, AT_REMOVEDIR);
-#endif
-	return __syscall_ret(r);
+	int r = zsys_unlink(path);
+	if (r < 0 && errno == EISDIR) r = zsys_rmdir(path);
+	return r;
 }

72a9b375e0f22880d62f789a868b0d91fe8500fe
Fil Pizlo <pizlo@mac.com>
2024-08-06 17:13:09 -0700

    Implement prctl syscall. Corresponds to llvm-project-deluge/39f83eac96ee

--- src/linux/prctl.c
+++ src/linux/prctl.c
@@ -4,11 +4,5 @@
 
 int prctl(int op, ...)
 {
-	unsigned long x[4];
-	int i;
-	va_list ap;
-	va_start(ap, op);
-	for (i=0; i<4; i++) x[i] = va_arg(ap, unsigned long);
-	va_end(ap);
-	return syscall(SYS_prctl, op, x[0], x[1], x[2], x[3]);
+	return (int)zcall_int(zsys_prctl, zargs());
 }

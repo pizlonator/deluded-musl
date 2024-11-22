db67208bad16f2c1fc91dbbea95f5bbd2fd1c296
Fil Pizlo <pizlo@mac.com>
2024-11-22 13:44:48 -0800

    Implement madvise. Corresponds to llvm-project-deluge/8d1e47a026ea

--- src/mman/madvise.c
+++ src/mman/madvise.c
@@ -3,7 +3,7 @@
 
 int __madvise(void *addr, size_t len, int advice)
 {
-	return syscall(SYS_madvise, addr, len, advice);
+	return zsys_madvise(addr, len, advice);
 }
 
 weak_alias(__madvise, madvise);

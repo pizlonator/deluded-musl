8a4323b0c781fad9dacefa3ebd3921c8d202336a
Fil Pizlo <pizlo@mac.com>
2025-02-13 17:15:54 -0800

    Adopt looser longjmp semantics. Corresponds to llvm-project-deluge/4c8a979ec846.

--- src/setjmp/longjmp.c
+++ src/setjmp/longjmp.c
@@ -3,15 +3,15 @@
 
 _Noreturn void longjmp(jmp_buf buf, int value)
 {
-    zlongjmp((zjmp_buf*)buf[0].__p, value);
+    zlongjmp(*(zjmp_buf**)buf, value);
 }
 
 _Noreturn void _longjmp(jmp_buf buf, int value)
 {
-    z_longjmp((zjmp_buf*)buf[0].__p, value);
+    zlongjmp(*(zjmp_buf**)buf, value);
 }
 
 _Noreturn void siglongjmp(sigjmp_buf buf, int value)
 {
-    zsiglongjmp((zjmp_buf*)buf[0].__p, value);
+    zlongjmp(*(zjmp_buf**)buf, value);
 }

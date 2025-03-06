e0a806ff01646e4cfb27ae63dc07b3bd251676e7
Fil Pizlo <pizlo@mac.com>
2025-03-05 16:15:29 -0800

    Implement sigqueue syscall. Corresponds to llvm-project-deluge/9eb8f05799c6

--- src/signal/sigqueue.c
+++ src/signal/sigqueue.c
@@ -6,17 +6,5 @@
 
 int sigqueue(pid_t pid, int sig, const union sigval value)
 {
-	siginfo_t si;
-	sigset_t set;
-	int r;
-	memset(&si, 0, sizeof si);
-	si.si_signo = sig;
-	si.si_code = SI_QUEUE;
-	si.si_value = value;
-	si.si_uid = getuid();
-	__block_app_sigs(&set);
-	si.si_pid = getpid();
-	r = syscall(SYS_rt_sigqueueinfo, pid, sig, &si);
-	__restore_sigs(&set);
-	return r;
+	return zsys_sigqueue(pid, sig, value.sival_ptr);
 }

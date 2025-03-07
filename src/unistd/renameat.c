fefc9ef96ccfe5dbfec82cdce639b33ec90824ad
Fil Pizlo <pizlo@mac.com>
2025-03-07 10:10:15 -0800

    Add statfs/statvfs (and friends), renameat, and symlinkat. Corresponds to llvm-project-deluge/6e983aa5cf8e

--- src/unistd/renameat.c
+++ src/unistd/renameat.c
@@ -3,9 +3,5 @@
 
 int renameat(int oldfd, const char *old, int newfd, const char *new)
 {
-#ifdef SYS_renameat
-	return syscall(SYS_renameat, oldfd, old, newfd, new);
-#else
-	return syscall(SYS_renameat2, oldfd, old, newfd, new, 0);
-#endif
+	return zsys_renameat(oldfd, old, newfd, new);
 }

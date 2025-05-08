d49fbd4fce3c05c6399e098f84586d3428d015a9
Fil Pizlo <pizlo@mac.com>
2025-05-08 10:41:09 -0700

    Add some syscalls.h

--- src/network/netlink.c
+++ src/network/netlink.c
@@ -47,6 +47,6 @@ int __rtnetlink_enumerate(int link_af, int addr_af, int (*cb)(void *ctx, struct 
 	if (fd < 0) return -1;
 	r = __netlink_enumerate(fd, 1, RTM_GETLINK, link_af, cb, ctx);
 	if (!r) r = __netlink_enumerate(fd, 2, RTM_GETADDR, addr_af, cb, ctx);
-	__syscall(SYS_close,fd);
+	zsys_close(fd);
 	return r;
 }
--- src/unistd/dup3.c
+++ src/unistd/dup3.c
@@ -6,21 +6,7 @@
 
 int __dup3(int old, int new, int flags)
 {
-	int r;
-#ifdef SYS_dup2
-	if (old==new) return __syscall_ret(-EINVAL);
-	if (flags) {
-		while ((r=__syscall(SYS_dup3, old, new, flags))==-EBUSY);
-		if (r!=-ENOSYS) return __syscall_ret(r);
-		if (flags & ~O_CLOEXEC) return __syscall_ret(-EINVAL);
-	}
-	while ((r=__syscall(SYS_dup2, old, new))==-EBUSY);
-	if (r >= 0 && (flags & O_CLOEXEC))
-		__syscall(SYS_fcntl, new, F_SETFD, FD_CLOEXEC);
-#else
-	while ((r=__syscall(SYS_dup3, old, new, flags))==-EBUSY);
-#endif
-	return __syscall_ret(r);
+	return zsys_dup3(old, new, flags);
 }
 
 weak_alias(__dup3, dup3);

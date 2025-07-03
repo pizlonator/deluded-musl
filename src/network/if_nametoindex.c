f6ba03a8268664ef054529e796663f91c5d53c6c
Fil Pizlo <pizlo@mac.com>
2025-07-03 15:02:00 -0700

    Make if_nametoindex work

--- src/network/if_nametoindex.c
+++ src/network/if_nametoindex.c
@@ -13,6 +13,6 @@ unsigned if_nametoindex(const char *name)
 	if ((fd = socket(AF_UNIX, SOCK_DGRAM|SOCK_CLOEXEC, 0)) < 0) return 0;
 	strncpy(ifr.ifr_name, name, sizeof ifr.ifr_name);
 	r = ioctl(fd, SIOCGIFINDEX, &ifr);
-	__syscall(SYS_close, fd);
+	zsys_close(fd);
 	return r < 0 ? 0 : ifr.ifr_ifindex;
 }

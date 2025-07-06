dd052e6dddc527fb75959ca26d7241c11f08b787
Fil Pizlo <pizlo@mac.com>
2025-07-05 21:19:43 -0700

    Make readlinkat work

--- src/unistd/readlinkat.c
+++ src/unistd/readlinkat.c
@@ -3,12 +3,5 @@
 
 ssize_t readlinkat(int fd, const char *restrict path, char *restrict buf, size_t bufsize)
 {
-	char dummy[1];
-	if (!bufsize) {
-		buf = dummy;
-		bufsize = 1;
-	}
-	int r = __syscall(SYS_readlinkat, fd, path, buf, bufsize);
-	if (buf == dummy && r > 0) r = 0;
-	return __syscall_ret(r);
+	return zsys_readlinkat(fd, path, buf, bufsize);
 }

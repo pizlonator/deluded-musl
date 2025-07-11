f6d6e800fa59e2e732199b3971b1fed473fdb6cd
Fil Pizlo <pizlo@mac.com>
2025-07-10 18:14:50 -0700

    Add mknod and mknodat

--- src/stat/mknod.c
+++ src/stat/mknod.c
@@ -4,9 +4,5 @@
 
 int mknod(const char *path, mode_t mode, dev_t dev)
 {
-#ifdef SYS_mknod
-	return syscall(SYS_mknod, path, mode, dev);
-#else
-	return syscall(SYS_mknodat, AT_FDCWD, path, mode, dev);
-#endif
+	return zsys_mknod(path, mode, dev);
 }

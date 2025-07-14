f44b5872fb6052c32262e077f7cf4b874f8673a4
Fil Pizlo <pizlo@mac.com>
2025-07-13 18:17:25 -0700

    Make mount/umount work

--- src/linux/mount.c
+++ src/linux/mount.c
@@ -3,15 +3,15 @@
 
 int mount(const char *special, const char *dir, const char *fstype, unsigned long flags, const void *data)
 {
-	return syscall(SYS_mount, special, dir, fstype, flags, data);
+        return zsys_mount(special, dir, fstype, flags, data);
 }
 
 int umount(const char *special)
 {
-	return syscall(SYS_umount2, special, 0);
+        return zsys_umount2(special, 0);
 }
 
 int umount2(const char *special, int flags)
 {
-	return syscall(SYS_umount2, special, flags);
+        return zsys_umount2(special, flags);
 }
